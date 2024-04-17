5705b3b27079a3043e9f2382ef1156ae26dc221b
DoVanTuan285 <dovantuan285@gmail.com>
2024-04-17 16:43:24 +0700

    [REMOVE] remove lib dont use

--- application/sources/libraries/Makefile.mk
+++
@@ -1,3 +0,0 @@
-include sources/libraries/QRCode/Makefile.mk
-include sources/libraries/ArduinoJson/Makefile.mk
-include sources/libraries/nlohmann/Makefile.mk
--- application/sources/libraries/QRCode/Makefile.mk
+++
@@ -1,6 +0,0 @@
-CFLAGS += -I./sources/libraries/QRCode/src
-CPPFLAGS += -I./sources/libraries/QRCode/src
-
-VPATH += sources/libraries/QRCode/src
-
-SOURCES += sources/libraries/QRCode/src/qrcode.c
